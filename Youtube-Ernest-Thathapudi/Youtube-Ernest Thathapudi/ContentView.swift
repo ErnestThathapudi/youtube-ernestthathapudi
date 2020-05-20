69bb85b5838cd0e7222c2762a335fd9ec70da6b6
Ernest Thathapudi <65647319+ErnestThathapudi@users.noreply.github.com>
2020-05-20 14:09:05 +0530

    Add files via upload

---
+++ Youtube-Ernest-Thathapudi/Youtube-Ernest Thathapudi/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Youtube-Ernest Thathapudi
+//
+//  Created by ERNEST THATHAPUDI on 20/05/20.
+//  Copyright © 2020 ERNEST THATHAPUDI. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
